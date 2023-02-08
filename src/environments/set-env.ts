bce5cb941ced763afecc9a935a63411f911a33af
AminaChakirDev <amina.aitm@gmail.com>
2023-02-08 23:51:11 +0100

    change set-env

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -4,22 +4,16 @@ const setEnv = () => {
   // Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
   // Load node modules
-  const colors = require('colors');
-  const appVersion = require('../../package.json').version;
   require('dotenv').config({
     path: 'src/environments/.env',
   });
   // `environment.ts` file structure
   const envConfigFile = `export const environment = {
-  apiKey: '${process.env?.['API_KEY']}',
-  appVersion: '${appVersion}',
-  production: true,
+  apiKey: '${process.env?.['API_KEY']}'
 };
 `;
   console.log(
-    colors.magenta(
-      'The file `environment.ts` will be written with the following content: \n'
-    )
+    'The file `environment.ts` will be written with the following content: \n'
   );
   writeFile(targetPath, envConfigFile, (err: Error) => {
     if (err) {
@@ -27,9 +21,7 @@ const setEnv = () => {
       throw err;
     } else {
       console.log(
-        colors.magenta(
-          `Angular environment.ts file generated correctly at ${targetPath} \n`
-        )
+        `Angular environment.ts file generated correctly at ${targetPath} \n`
       );
     }
   });
